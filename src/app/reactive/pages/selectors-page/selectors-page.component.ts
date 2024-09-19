b1a1b2831dd44440ec8186b3b46ee20072337a9d
Alejandro Mariño <alejandromf_199@hotmail.com>
2024-09-19 13:57:51 +0200

    v1.0.8 => Countries and Borders selector added. Bug on borders selector.

--- src/app/reactive/pages/selectors-page/selectors-page.component.ts
+++ src/app/reactive/pages/selectors-page/selectors-page.component.ts
@@ -1,26 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
-import { Region } from '../../interfaces/country.interface';
+import { Country, Region } from '../../interfaces/country.interface';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
 	selector: 'app-selectors-page',
 	templateUrl: './selectors-page.component.html',
 	styleUrl: './selectors-page.component.css'
 })
-export class SelectorsPageComponent {
+export class SelectorsPageComponent implements OnInit {
 	public selectorForm: FormGroup = this.fb.group({
 		continent: ['', Validators.required],
 		country: ['', Validators.required],
 		borders: ['', Validators.required]
 	});
+	public countriesByRegion: Country[] = [];
+	public bordersByCountries: string[] = [];
 
 	constructor(
 		private fb: FormBuilder,
 		private countryService: CountryService
 	) {}
 
+	ngOnInit(): void {
+		this.onContinentChange();
+	}
+
 	get regions(): Region[] {
 		return this.countryService.getRegions;
+	}
+
+	onContinentChange(): void {
+		this.selectorForm
+			.get('continent')
+			?.valueChanges.pipe(
+				tap(() => this.selectorForm.controls['country'].setValue('')),
+				switchMap(
+					(
+						continent // El switchMap recibe la data de un observabble para devolversela a otro Observable.
+					) => this.countryService.getCountriesByContinent(continent)
+				)
+			)
+			.subscribe((countries) => (this.countriesByRegion = countries));
+	}
+
+	onCountryChange(): void {
+		this.selectorForm
+			.get('country')
+			?.valueChanges.pipe(
+				tap(() => this.selectorForm.controls['borders'].setValue('')),
+				filter((value: string) => value.length > 0), // El filter si retorna un true continua con lo siguiente, si recibe un false para el código ahí.
+				switchMap((alphaCode) =>
+					this.countryService.getBordersByCountry(alphaCode)
+				)
+			)
+			.subscribe((borders) => {
+				if (!borders.borders) {
+					this.bordersByCountries = [];
+					return;
+				}
+
+				this.bordersByCountries = borders.borders;
+			});
 	}
 }
--- src/app/reactive/services/country.service.ts
+++ src/app/reactive/services/country.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interface';
+import { Country, Region } from '../interfaces/country.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountryService {
@@ -11,9 +13,25 @@ export class CountryService {
 		Region.Oceania
 	];
 
-	constructor() {}
+	constructor(private http: HttpClient) {}
 
 	get getRegions(): Region[] {
 		return [...this._regions];
+	}
+
+	getCountriesByContinent(continent: Region): Observable<Country[]> {
+		if (!continent) return of([]);
+		return this.http
+			.get<
+				Country[]
+			>(`https://restcountries.com/v3.1/region/${continent}`)
+			.pipe(tap((res) => console.log(res)));
+	}
+
+	getBordersByCountry(alphaCode: string): Observable<Country> {
+		if (!alphaCode) return of();
+		return this.http
+			.get<Country>(`https://restcountries.com/v3.1/alpha/${alphaCode}`)
+			.pipe(tap((res) => console.log(res)));
 	}
 }
